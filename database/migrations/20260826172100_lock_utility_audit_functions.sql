203f420093aca390f5bba29e4d65359e8be9229b
Felipe Vieira <alexandrefelipe210408@gmail.com>
2026-09-03 11:39:31 -0300

    chore: remover migration legada de utilidades

--- database/migrations/20260826172100_lock_utility_audit_functions.sql
+++
@@ -1,4 +0,0 @@
-revoke all on function public.log_utility_meter_insert() from public, anon, authenticated;
-revoke all on function public.log_utility_reading_insert() from public, anon, authenticated;
-grant execute on function public.log_utility_meter_insert() to postgres;
-grant execute on function public.log_utility_reading_insert() to postgres;
